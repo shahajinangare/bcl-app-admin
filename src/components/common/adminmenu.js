65f7c7c746df55de010368598e5b5fc188143eec
npnileshpatil <npnileshpatil1986@gmail.com>
2018-06-21 11:22:30 +0530

    Menu

--- src/components/common/adminmenu.js
+++ src/components/common/adminmenu.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import '../../assets/stylesheets/menu.css';
 import ReactDOM from 'react-dom';
 import MainContent from '../../view/common/maincontent';
-class adminmenu extends Component {
+import MenuContent from '../../view/common/adminmenu';
+
+
+class adminmenu extends React.Component {
+    data = [];
+
     constructor(props) {
         super(props);
-         
-                this.state = {           
+
+        this.state = {
+            data: []
         };
-
         this.logoutsubmit = this.logoutsubmit.bind(this);
-    }
-    logoutsubmit(event) {
-        // alert('forgot pass');
-           ReactDOM.render((<MainContent />), document.getElementById("main-content"));  
+        //this.getmenubyrole = this.getmenubyrole.bind(this);
     }
 
-    getmenubyrole() {           
+    logoutsubmit(event) {
+        ReactDOM.render((<MainContent />), document.getElementById("main-content"));
+    }
+
+    componentDidMount() {
+        //   alert('componentDidMount');
         fetch('http://localhost:7000/admin/getmenubyrole', {
             method: 'POST',
             headers: {
@@ -30,59 +37,63 @@ class adminmenu extends Component {
             }),
         }).then((response) => response.json())
             .then((responseJson) => {
-                console.log(responseJson);
+                //  console.log(responseJson.result);
                 this.setState({
-                    ErrorMsg: responseJson.message
+                    data: responseJson.result
                 });
-      
-                responseJson.result.forEach(element => {                   
-                    
-                    console.log(element);
-                   
-                });
-               // return responseJson.result;
+                // console.log(this.state.data);
+                // responseJson.result.forEach(element => {
+                //    // console.log(element);
+                // });
+                // return responseJson.result;
             })
             .catch((error) => {
                 console.error(error);
             });
     }
 
-    getmenuhtml() {
-       
+
+
+
+    render() {
+        console.log('render data');
+        console.log(this.state.data);
+
         return (
- 
-            <div id='cssmenu'>
-                 <ul>
-                     <li><a href='#'><span>Home</span></a></li>
-                     <li className='active has-sub'><a href='#'><span>Products</span></a>
-                         <ul>
-                             <li className='has-sub'><a href='#'><span>Product 1</span></a>
-                                 <ul>
-                                     <li><a href='#'><span>Sub Product</span></a></li>
-                                     <li className='last'><a href='#'><span>Sub Product</span></a></li>
-                                 </ul>
+
+            <div id="menu-root">
+                <div id='cssmenu'>
+                    <ul>
+                        <li><a href='#'><span>Home</span></a></li>
+
+                    
+                            {this.state.data.map(itemmain => {
+
+                                if (itemmain.parentid == -1) {                              
+                                    return <li className='active has-sub' id={itemmain.id}>                                    
+                                    <a href='#'><span>{itemmain.menuname}</span></a>       
+                                    <ul>
+                              {this.state.data.map(subitem =>  {     
+                                if (subitem.parentid == itemmain.id && subitem.parentid != -1) {                                
+                                     return 
+                                       <li><a href='#' id={subitem.id} ><span>{subitem.menuname}</span></a></li>                                    
+                                  
+                                   }
+                                })}
+                                 </ul>                                   
                              </li>
-                             <li className='has-sub'><a href='#'><span>Product 2</span></a>
-                                 <ul>
-                                     <li><a href='#'><span>Sub Product</span></a></li>
-                                     <li className='last'><a href='#'><span>Sub Product</span></a></li>
-                                 </ul>
-                             </li>
-                         </ul>
-                     </li>
-                     <li><a href='#'><span>About</span></a></li>
-                     <li className='last'><a href='#'><span>Contact</span></a></li> 
-                     <li className='last'><a href='#' onClick={this.logoutsubmit} ><span>Log Out</span></a></li>
-                 </ul>
-             </div>
-            
+                               
+                            }
+                            }
+                            )}
+                 
+
+                    </ul>
+                </div>
+            </div>
+            // <MenuContent login={this}/>
         )
     }
-    render() {
-      return(
-      this.getmenuhtml()
-    );
-}
 }
 
 export default adminmenu;
--- src/view/common/adminmenu.jsx
+++ src/view/common/adminmenu.jsx
@@ -1,32 +1,20 @@
 import React from 'react';
 
 const MenuContent = ({menuaccess}) => (
-	<div id="menu-root">
-		<div id='cssmenu'>
-    <ul>
-        <li><a href='#'><span>Home</span></a></li>
-        <li className='active has-sub'><a href='#'><span>Products</span></a>
-            <ul>
-                <li className='has-sub'><a href='#'><span>Product 1</span></a>
-                    <ul>
-                        <li><a href='#'><span>Sub Product</span></a></li>
-                        <li className='last'><a href='#'><span>Sub Product</span></a></li>
-                    </ul>
-                </li>
-                <li className='has-sub'><a href='#'><span>Product 2</span></a>
-                    <ul>
-                        <li><a href='#'><span>Sub Product</span></a></li>
-                        <li className='last'><a href='#'><span>Sub Product</span></a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li><a href='#'><span>About</span></a></li>
-        <li className='last'><a href='#'><span>Contact</span></a></li>
-        <li className='last'><a href='#'  ><span>Log Out</span></a></li>
-    </ul>
-</div>
-</div>
+  //  console.log(menuaccess.state.data);
+     <div id="menu-root">
+
+      <ul>
+          <li>
+            <div>
+             {/* {this.menuaccess.options.map(item=>
+              <li>{item.name}</li> */}
+              )}
+            </div>
+          </li>
+        </ul> 
+    
+ </div>
 );
 
 export default MenuContent;
