9fe705f0ba318e87472ff889fb75280f7193a474
pravinphapale <pravin.phapale88@gmail.com>
2018-06-22 10:24:45 +0530

    con

--- src/components/common/maincontent.js
+++ src/components/common/maincontent.js
@@ -4,12 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from '../../components/users/login';
 import Register from '../../components/users/register';
 import Forgotpass from '../../components/users/forgotpass';
-<<<<<<< HEAD
 import { Redirect } from 'react-router'
-=======
-import HomeComponent from '../../components/common/home';
-
->>>>>>> ef244cbc96398ed5aae5cceb2b21002c3b84ac15
 class MainComponent extends Component {
   render() {
     return (
@@ -19,12 +14,7 @@ class MainComponent extends Component {
             <Route exact path='/login' component={Login}  />
             <Route exact path='/register' component={Register}  />
             <Route exact path='/forgotpass' component={Forgotpass} />
-<<<<<<< HEAD
             {/* <Redirect to="/register" /> */}
-=======
-            <Route exact path='/home' component={HomeComponent} />
-           
->>>>>>> ef244cbc96398ed5aae5cceb2b21002c3b84ac15
           </Switch>
         </Router>
         <MainContent />
