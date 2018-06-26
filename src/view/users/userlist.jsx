2976d057d78dc8f451580ef0e9dc04e0ab76a85d
npnileshpatil <npnileshpatil1986@gmail.com>
2018-06-26 03:36:05 -0700

    EditUser

--- src/view/users/userlist.jsx
+++ src/view/users/userlist.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { DataTable } from 'react-data-components';
+import ReactTable from "react-table";
 
-
-var columns = [
-    { title: 'userid', prop: 'userid' },
-    { title: 'NAME', prop: 'NAME' },
-    { title: 'emailid', prop: 'emailid' },
-    { title: 'mobileno', prop: 'mobileno' }
-];
+import "react-table/react-table.css";
 
   
 const UserListContent = ({userdata}) => (
@@ -15,15 +9,103 @@ const UserListContent = ({userdata}) => (
     
 <div>
 
-     <DataTable
-        className="container"
-        keys="userid"
-        columns={userdata.columns}
-        initialData={userdata.state.outData}
-        initialPageLength={5}
-        initialSortBy={{ prop: 'NAME', order: 'descending' }}
-        pageLengthOptions={[5, 20, 50]}
-    />
+  
+            <br />
+            <br />
+            <br />
+            <br />
+            <ReactTable
+                data={userdata.state.outData}
+                filterable
+                defaultFilterMethod={(filter, row) =>
+                    String(row[filter.id.toLowerCase]) === filter.value.toLowerCase}
+                columns={[
+                    {
+                        Header: "Name",
+                        accessor: "NAME",
+                        filterMethod: (filter, row) =>
+                            row[filter.id].startsWith(filter.value) ||
+                            row[filter.id].endsWith(filter.value)
+                        // filterAll: true
+                    },
+                    {
+                        Header: "Email",
+                        accessor: "emailid",
+                        filterMethod: (filter, row) =>
+                            row[filter.id].startsWith(filter.value) ||
+                            row[filter.id].endsWith(filter.value)
+
+
+                    },
+                    {
+                        Header: "Mobile No",
+                        accessor: "mobileno",
+                        filterMethod: (filter, row) =>
+                            row[filter.id].startsWith(filter.value) ||
+                            row[filter.id].endsWith(filter.value)
+
+                    },
+                    {
+                        Header: 'Actions',
+                        Cell:
+                            selectedrow => {
+                                if (selectedrow.original.isactive === 'N') {
+                                    return (
+                                        <div>
+                                            <a className="badge"
+                                                onClick={e => userdata.activeinactiveuser(selectedrow)}>Activate</a>
+                                        </div>
+                                    )
+                                }
+                                else {
+                                    return (
+                                        <div>
+                                            <a className="badge"
+                                                onClick={e => userdata.activeinactiveuser(selectedrow)}>Deactivate</a>
+                                        </div>
+                                    )
+                                }
+
+
+
+                            }
+                    },
+                    {
+                        Header: 'Unlock',
+                        Cell:
+                            selectedrow => {
+                                if (selectedrow.original.lockstatus === 'Y') {
+                                    return (
+                                        <div>
+                                            <a className="badge"
+                                                onClick={e => userdata.unlockuser(selectedrow)}>Unlock</a>
+                                        </div>
+                                    )
+                                }
+
+                            }
+                    },
+                    {
+                        Header: 'Edit',
+                        Cell:
+                            selectedrow => {
+                                return (
+                                    <div>
+                                        <a className="badge"
+                                            onClick={e => userdata.editusers(selectedrow)} >Edit</a>
+                                    </div>
+                                )
+
+
+                            }
+                    }
+
+                ]
+                }
+                defaultPageSize={10}
+                className="-striped -highlight"
+            />
+
     
 </div>
 );
